a47464d8037911f63102f7b2d23e352cc63f41fa
Nishu <nimish.prabhakar@eagleview.com>
2021-03-27 16:11:40 +0530

    New commit

---
+++ src/Request-Methods/GetRequest.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const GetRequest = () => {
+  const [data, setData] = useState({});
+
+  const clickHandler = (e) => {
+    const fetchData = new Promise((resolve, reject) => {
+      resolve(axios.get('https://jsonplaceholder.typicode.com/posts'));
+      reject('API Failing');
+    });
+
+    fetchData
+      .then((res) => {
+        setData(res.data[Math.floor(Math.random() * 100)]);
+      })
+      .catch((err) => console.log('failed to call API :', err));
+  };
+
+  return (
+    <>
+      <button onClick={clickHandler}>GET</button>
+      <ul>{data.title}</ul>
+    </>
+  );
+};
+
+export default GetRequest;
